e69676939286db57f15fba55a45ca1db34deda93
Lucy Follansbee <lfollansbee@gmail.com>
2019-12-06 10:24:03 -0700

    add create chirp endpoint

--- server/routes/chirp-router.js
+++ server/routes/chirp-router.js
@@ -3,7 +3,7 @@ import Chirp from '../models/Chirp';
 
 const router = Router()
 
-router.get('/', async function(req, res) {
+router.get('/', async function (req, res) {
   await Chirp.find({}, (err, chirps) => {
     if (err) {
       return res.status(400).json({ success: false, error: err })
@@ -16,5 +16,26 @@ router.get('/', async function(req, res) {
     return res.status(200).json({ success: true, data: chirps })
   }).catch(err => console.log(err))
 })
+
+router.post('/', async function (req, res) {
+  const chirp = new Chirp()
+  chirp.text = req.body.text;
+
+  chirp.save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: chirp._id,
+        message: 'Chirp created!',
+      })
+    })
+    .catch(error => {
+      return res.status(400).json({
+        error,
+        message: 'Chirp not created!',
+      })
+    })
+})
+
 
 export default router
